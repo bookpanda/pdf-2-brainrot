64799fa9abac0b6f4d0e6296d06b29777641edbe
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-01 18:01:52 +0700

    init front

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,5 @@
 import uvicorn
 from app.config import settings
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # if run.py is called directly, not imported
     uvicorn.run("app.main:app", host="0.0.0.0", port=settings.port, reload=True)
