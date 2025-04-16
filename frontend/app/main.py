374bbad2bea45c8057f91d2f35d7488e2c8d2a53
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-16 23:31:00 +0700

    ui upaod

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -14,28 +14,31 @@ st.write("Hi yall! This is a simple app that converts PDF files to brainrot.")
 st.write("Upload your PDF file below:")
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
+if "uploaded_key" not in st.session_state:
+    st.session_state.uploaded_key = None
+
 if uploaded_file is not None:
     st.write(f"File uploaded: {uploaded_file.name}")
 
-    if st.button("Convert to Brainrot Shorts"):
-        presigned_url, key = get_presigned_url(uploaded_file.name)
-        print(f"Presigned URL: {presigned_url}, Key: {key}")
-        file_bytes = uploaded_file.getvalue()
+    if st.session_state.uploaded_key != uploaded_file.name:
+        if st.button("Convert to Brainrot Shorts"):
+            presigned_url, key = get_presigned_url(uploaded_file.name)
+            file_bytes = uploaded_file.getvalue()
 
-        response = requests.put(
-            presigned_url,
-            data=file_bytes,
-            headers={"Content-Type": "application/pdf"},
-        )
+            response = requests.put(
+                presigned_url,
+                data=file_bytes,
+                headers={"Content-Type": "application/pdf"},
+            )
 
-        print(f"response: {response.content}")
-
-        if response.status_code == 200:
-            st.success("Successfully converted to Brainrot Shorts!")
-            st.write(f"Key: {key}")
-            # st.write(response.json().get("brainrot_shorts"))
-        else:
-            st.error(f"Conversion failed, status code: {response.status_code}")
+            if response.status_code == 200:
+                st.session_state.uploaded_key = uploaded_file.name  # mark as processed
+                st.success("Converting to Brainrot Shorts! Please wait...")
+                st.write(f"Key: {key}")
+            else:
+                st.error(f"Conversion failed, status code: {response.status_code}")
+    else:
+        st.warning("This file has already been processed. Please upload a new one.")
 
 
 # ---- Display Processed Videos ----
