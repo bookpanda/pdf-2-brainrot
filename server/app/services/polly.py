5c7697a253302df6da262ac7711a771f8275edf7
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-16 22:45:47 +0700

    polly upload to s3

--- server/app/services/polly.py
+++ server/app/services/polly.py
@@ -1,19 +1,32 @@
 import boto3
+from app.config import settings
+from app.constants import AUDIO_UPLOAD_FOLDER
+from app.data.s3 import upload_file_to_s3
 from botocore.exceptions import BotoCoreError, ClientError
 
 polly_client = boto3.client("polly", region_name="ap-southeast-1")
 s3_client = boto3.client("s3", region_name="ap-southeast-1")
 
 
-def generate_voice_and_mark(text):
+def generate_voice_and_mark(text, s3_key_prefix: str = AUDIO_UPLOAD_FOLDER):
     try:
         voice_response = polly_client.synthesize_speech(
             Text=text, TextType="text", OutputFormat="mp3", VoiceId="Joanna"
         )
+        audio_stream = voice_response["AudioStream"].read()
+        audio_key = f"{s3_key_prefix}/raw.mp3"
 
-        with open("raw.mp3", "wb") as f:
-            f.write(voice_response["AudioStream"].read())
-        print("Audio saved successfully.")
+        s3_client.put_object(
+            Bucket=settings.AWS_BUCKET_NAME,
+            Key=audio_key,
+            Body=audio_stream,
+            ContentType="audio/mpeg",
+        )
+        print(f"Audio uploaded to S3: {audio_key}")
+
+        # with open("./generated/raw.mp3", "wb") as f:
+        #     f.write(voice_response["AudioStream"].read())
+        # print("Audio saved successfully.")
 
     except (BotoCoreError, ClientError) as e:
         print("Failed to generate or save audio:", e)
@@ -28,7 +41,7 @@ def generate_voice_and_mark(text):
             SpeechMarkTypes=["word"],
         )
 
-        with open("mark.marks", "wb") as f:
+        with open("./generated/mark.marks", "wb") as f:
             f.write(mark_response["AudioStream"].read())
         print("Speech marks saved successfully.")
 
