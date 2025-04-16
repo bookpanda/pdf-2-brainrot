075f13699238cc7870b444d344d4a214412a9526
Idhibhat Pankam <marginpankam@gmail.com>
2025-04-16 23:23:29 +0700

    poly no upload

--- server/app/services/polly.py
+++ server/app/services/polly.py
@@ -1,7 +1,5 @@
 import boto3
-from app.config import settings
 from app.constants import AUDIO_UPLOAD_FOLDER
-from app.data.s3 import upload_file_to_s3
 from botocore.exceptions import BotoCoreError, ClientError
 
 polly_client = boto3.client("polly", region_name="ap-southeast-1")
@@ -16,13 +14,13 @@ def generate_voice_and_mark(text, s3_key_prefix: str = AUDIO_UPLOAD_FOLDER):
         audio_stream = voice_response["AudioStream"].read()
         audio_key = f"{s3_key_prefix}/raw.mp3"
 
-        s3_client.put_object(
-            Bucket=settings.AWS_BUCKET_NAME,
-            Key=audio_key,
-            Body=audio_stream,
-            ContentType="audio/mpeg",
-        )
-        print(f"Audio uploaded to S3: {audio_key}")
+        # s3_client.put_object(
+        #     Bucket=settings.AWS_BUCKET_NAME,
+        #     Key=audio_key,
+        #     Body=audio_stream,
+        #     ContentType="audio/mpeg",
+        # )
+        # print(f"Audio uploaded to S3: {audio_key}")
 
         with open("./generated/raw.mp3", "wb") as f:
             f.write(audio_stream)
